c6b6b09635a8e1068f8fae8f9545b6c3d15e3ea4
Robert Hosbach <robert.hosbach@gmail.com>
2020-01-20 13:16:02 -0800

    Add test file for tests of utils.validate_datetime().

---
+++ tests/utils/test_validate_datetime.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+from hypothesis import given
+from hypothesis.strategies import datetimes
+import pytest
+
+from pysol.utils import validate_datetime
+
+
+@pytest.mark.utils
+@given(datetimes())
+def test_validate_datetime(dt):
+    """Functional test to ensure the validate_datetime() method
+    runs properly on naive datetime objects."""
+    assert isinstance(validate_datetime(dt), datetime)
+
+
+@pytest.mark.utils
+def test_invalid_type():
+    with pytest.raises(TypeError):
+        assert validate_datetime(123)
+
+
+@ pytest.mark.utils
+def test_valid_string():
+    """Test to ensure validate_datetime() can parse a typical
+    datetime string."""
+    assert isinstance(validate_datetime("January 1, 2019 12:00 PM"), datetime)
+
+
+@pytest.mark.utils
+def test_invalid_string():
+    """Test to ensure validate_datetime() will throw a 
+    ValueError with an invalid string input."""
+    with pytest.raises(ValueError):
+        assert validate_datetime("January 1, blah blah blah")
